b9a9e723e062ab82081a95bb11fcf1b67cf0044d
minhquangkx <quangptm157@gmail.com>
2025-08-30 00:31:35 +0700

    update URL api AI

--- src/pages/admin/CarPredict.jsx
+++ src/pages/admin/CarPredict.jsx
@@ -3,7 +3,7 @@ import AdminSidebar from "../../components/admin/AdminSidebar";
 import AdminTopbar from "../../components/admin/AdminTopbar";
 
 // Base URL của Flask API
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'http://127.0.0.1:5050';
 
 const CarPredictForm = () => {
     // State cho dữ liệu form
